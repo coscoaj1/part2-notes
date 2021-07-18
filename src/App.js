17b5658763e5406b06edbfd6e09e81e89d4d2ab3
Anthony Cosco <coscoanthony1@gmail.com>
2021-07-18 17:25:53 -0400

    axios

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import Note from './components/Note';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const App = (props) => {
-	const [notes, setNotes] = useState(props.notes);
-	const [newNote, setNewNote] = useState('a new note');
+const App = () => {
+	const [notes, setNotes] = useState([]);
+	const [newNote, setNewNote] = useState('');
+	const [showAll, setShowAll] = useState(true);
 
-	const addNote = (event) => {
-		event.preventDefault();
-		const noteObject = {
-			content: newNote,
-			date: new Date().toISOString(),
-			important: Math.random() < 0.5,
-			id: notes.length + 1,
-		};
-
-		setNotes(notes.concat(noteObject));
-		setNewNote('');
-	};
-
-	const handleNoteChange = (event) => {
-		console.log(event.target.value);
-		setNewNote(event.target.value);
-	};
+	useEffect(() => {
+		console.log('effect');
+		axios.get('http://localhost:3001/notes').then((response) => {
+			console.log('promise fulfilled');
+			setNotes(response.data);
+		});
+	}, []);
+	console.log('render', notes.length, 'notes');
 
 	return (
 		<div>
@@ -31,10 +24,6 @@ const App = (props) => {
 				{notes.map((note) => (
 					<Note key={note.id} note={note} />
 				))}
-				<form onSubmit={addNote}>
-					<input value={newNote} onChange={handleNoteChange} />
-					<button type="submit">save</button>
-				</form>
 			</ul>
 		</div>
 	);
