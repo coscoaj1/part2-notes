216221b22ae18b9ad90468a92caaf4c40ce86f29
Anthony Cosco <coscoanthony1@gmail.com>
2021-07-24 17:53:29 -0400

    notes updating server/toggling show importance

--- src/App.js
+++ src/App.js
@@ -6,24 +6,69 @@ import axios from 'axios';
 const App = () => {
 	const [notes, setNotes] = useState([]);
 	const [newNote, setNewNote] = useState('');
-	const [showAll, setShowAll] = useState(true);
+	const [showAll, setShowAll] = useState(false);
 
-	useEffect(() => {
+	const addNote = (event) => {
+		event.preventDefault();
+		const noteObject = {
+			content: newNote,
+			date: new Date(),
+			important: Math.random() < 0.5,
+		};
+
+		axios.post('http://localhost:3001/notes', noteObject).then((response) => {
+			console.log(response.data);
+			setNotes(notes.concat(response.data));
+			setNewNote('');
+		});
+	};
+
+	const hook = () => {
 		console.log('effect');
 		axios.get('http://localhost:3001/notes').then((response) => {
 			console.log('promise fulfilled');
 			setNotes(response.data);
 		});
-	}, []);
+	};
+
+	useEffect(hook, []);
 	console.log('render', notes.length, 'notes');
 
+	const handleNoteChange = (event) => {
+		setNewNote(event.target.value);
+	};
+
+	const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+	const toggleImportanceOf = (id) => {
+		const url = `http://localhost:3001/notes/${id}`;
+		const note = notes.find((n) => n.id === id);
+		const changedNote = { ...note, important: !note.important };
+
+		axios.put(url, changedNote).then((response) => {
+			setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+		});
+	};
 	return (
 		<div>
 			<h1>Notes</h1>
+			<div>
+				<button onClick={() => setShowAll(!showAll)}>
+					show {showAll ? 'important' : 'all'}
+				</button>
+			</div>
 			<ul>
-				{notes.map((note) => (
-					<Note key={note.id} note={note} />
+				{notesToShow.map((note, i) => (
+					<Note
+						key={i}
+						note={note}
+						toggleImportance={() => toggleImportanceOf(note.id)}
+					/>
 				))}
+				<form onSubmit={addNote}>
+					<input value={newNote} onChange={handleNoteChange} />
+					<button type="submit">save</button>
+				</form>
 			</ul>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import axios from 'axios';
 import React from 'react';
 
 ReactDOM.render(<App />, document.getElementById('root'));
