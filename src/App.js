c643e3e46acd895fc6385c2733a4c88cff3c3c89
Anthony Cosco <coscoanthony1@gmail.com>
2021-07-25 07:53:42 -0400

    added services

--- src/App.js
+++ src/App.js
@@ -2,25 +2,46 @@ import React from 'react';
 import Note from './components/Note';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import noteService from './services/notes';
 
 const App = () => {
 	const [notes, setNotes] = useState([]);
 	const [newNote, setNewNote] = useState('');
 	const [showAll, setShowAll] = useState(false);
 
+	useEffect(() => {
+		noteService //
+			.getAll()
+			.then((initialNotes) => {
+				setNotes(initialNotes);
+			});
+	}, []);
+
+	const toggleImportanceOf = (id) => {
+		const note = notes.find((n) => n.id === id);
+		const changedNote = { ...note, important: !note.important };
+
+		noteService //
+			.update(id, changedNote)
+			.then((returnedNote) => {
+				setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+			});
+	};
+
 	const addNote = (event) => {
 		event.preventDefault();
 		const noteObject = {
 			content: newNote,
-			date: new Date(),
+			date: new Date().toISOString(),
 			important: Math.random() < 0.5,
 		};
 
-		axios.post('http://localhost:3001/notes', noteObject).then((response) => {
-			console.log(response.data);
-			setNotes(notes.concat(response.data));
-			setNewNote('');
-		});
+		noteService //
+			.create(noteObject)
+			.then((returnedNote) => {
+				setNotes(notes.concat(returnedNote));
+				setNewNote('');
+			});
 	};
 
 	const hook = () => {
@@ -40,15 +61,6 @@ const App = () => {
 
 	const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
-	const toggleImportanceOf = (id) => {
-		const url = `http://localhost:3001/notes/${id}`;
-		const note = notes.find((n) => n.id === id);
-		const changedNote = { ...note, important: !note.important };
-
-		axios.put(url, changedNote).then((response) => {
-			setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
-		});
-	};
 	return (
 		<div>
 			<h1>Notes</h1>
