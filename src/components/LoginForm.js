63a2b3a50eae0c7e74e74971e26dba704479c8cd
Anthony Cosco <coscoanthony1@gmail.com>
2021-08-21 09:03:20 -0400

    added ESlint

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const LoginForm = ({
 	username,
 	password,
@@ -25,5 +27,13 @@ const LoginForm = ({
 			</div>
 		</div>
 	);
+};
+
+LoginForm.propTypes = {
+	handleSubmit: PropTypes.func.isRequired,
+	handleUsernameChange: PropTypes.func.isRequired,
+	handlePasswordChange: PropTypes.func.isRequired,
+	username: PropTypes.string.isRequired,
+	password: PropTypes.string.isRequired,
 };
 export default LoginForm;
